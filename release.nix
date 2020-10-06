ff7254d39fc8170ad9471c7b0de41a1947457868
Stefan Matting <smatting@users.noreply.github.com>
2020-10-06 13:47:28 +0200

    Update release.nix

--- release.nix
+++ release.nix
@@ -2,5 +2,4 @@ let
   src = import ./nix/sources.nix;
   pkgs = import src.nixpkgs {};
 in
-# import ./default.nix { inherit pkgs; }
-import ./default.nix { pkgs = import <nixpkgs> {}; }
+import ./default.nix { inherit pkgs; }
